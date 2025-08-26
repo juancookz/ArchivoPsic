eb1774150c59857a5beaeee16ce7eed1240d551e
juancookz <jcavila383@gmail.com>
2025-08-26 13:11:36 -0300

    Add Archivo and Seccion model classes

---
+++ Models/Archivo.cs
@@ -0,0 +1,7 @@
+public class Archivo
+{
+    public int Id { get; set; }
+    public int SeccionId { get; set; }
+    public string Titulo { get; set; }
+    public string RutaArchivo { get; set; }
+}
---
+++ Models/Seccion.cs
@@ -0,0 +1,6 @@
+public class Seccion
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+}
